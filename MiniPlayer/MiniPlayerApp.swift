764d00bb8a3862057daa3ad09bea71a6681f7b48
jongwoon-100 <jongwoon_100@linecorp.com>
2022-02-14 21:45:08 +0900

    first commit

---
+++ MiniPlayer/MiniPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MiniPlayerApp.swift
+//  MiniPlayer
+//
+//  Created by JongWoon on 2022/02/14.
+//
+
+import SwiftUI
+
+@main
+struct MiniPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
